b19515fc054a9d248b438d08166f15316289d17e
igaramadana <igrmdns085@gmail.com>
2025-05-29 13:15:26 +0700

    fix(components): increase modal width for better content display

--- resources/views/components/detail-button-kampus.blade.php
+++ resources/views/components/detail-button-kampus.blade.php
@@ -9,11 +9,10 @@
         <span class="sr-only">Detail</span>
     </button>
 
-
     <!-- Modal Detail -->
     <div id="detail-modal-{{ $kampus_id }}" tabindex="-1" aria-hidden="true"
         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur-sm bg-gray-900/50 dark:bg-gray-900/80">
-        <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative p-4 w-full max-w-xl max-h-full">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
                 <!-- Modal header -->
