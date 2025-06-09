bbb4aacb4ffccb8ec5a63593047eef3b029ddfde
igaramadana <igrmdns085@gmail.com>
2025-06-10 00:23:56 +0700

    feat(theme): add dark mode support and theme toggle

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -19,6 +19,15 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <script>
+        // Cek tema saat pertama kali load
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    </script>
 </head>
 
 <body class="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
@@ -36,6 +45,45 @@
     <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
     <script>
         AOS.init();
+    </script>
+    <script>
+        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+        // Change the icons inside the button based on previous settings
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            themeToggleLightIcon.classList.remove('hidden');
+        } else {
+            themeToggleDarkIcon.classList.remove('hidden');
+        }
+
+        var themeToggleBtn = document.getElementById('theme-toggle');
+
+        themeToggleBtn.addEventListener('click', function() {
+            // toggle icons inside button
+            themeToggleDarkIcon.classList.toggle('hidden');
+            themeToggleLightIcon.classList.toggle('hidden');
+
+            // if set via local storage previously
+            if (localStorage.getItem('color-theme')) {
+                if (localStorage.getItem('color-theme') === 'light') {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                } else {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                }
+            } else {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                } else {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                }
+            }
+        });
     </script>
     @stack('scripts')
     @if (session('toast_success'))
--- resources/views/layouts/admin/partials/header.blade.php
+++ resources/views/layouts/admin/partials/header.blade.php
@@ -19,13 +19,102 @@
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TOEIC</span>
                 </a>
             </div>
-            <div class="flex items-center">
-                <div class="hidden items-center mt-1 cursor-pointer sm:flex lg:flex lg:justify-center sm:justify-end">
-                    @auth
-                        <h1 class="font-semibold text-gray-900 dark:text-white">{{ auth()->user()->admin->admin_nama }}</h1>
-                    @endauth
+
+            <div class="flex items-center space-x-2 rtl:space-x-reverse">
+                <!-- Language Switch - Ikon saja -->
+                <div class="relative">
+                    <button type="button" data-dropdown-toggle="language-dropdown-menu"
+                        class="p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+                        @if (app()->getLocale() == 'en')
+                            <svg class="w-5 h-5 rounded-full" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 3900 3900">
+                                <path fill="#b22234" d="M0 0h7410v3900H0z" />
+                                <path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff"
+                                    stroke-width="300" />
+                                <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
+                                <g fill="#fff">
+                                    <g id="d">
+                                        <g id="c">
+                                            <g id="e">
+                                                <g id="b">
+                                                    <path id="a"
+                                                        d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z" />
+                                                    <use xlink:href="#a" y="420" />
+                                                    <use xlink:href="#a" y="840" />
+                                                    <use xlink:href="#a" y="1260" />
+                                                </g>
+                                                <use xlink:href="#a" y="1680" />
+                                            </g>
+                                            <use xlink:href="#b" x="247" y="210" />
+                                        </g>
+                                        <use xlink:href="#c" x="494" />
+                                    </g>
+                                    <use xlink:href="#d" x="988" />
+                                    <use xlink:href="#c" x="1976" />
+                                    <use xlink:href="#e" x="2470" />
+                                </g>
+                            </svg>
+                        @else
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg"
+                                class="w-5 h-5 rounded-full" alt="indonesia">
+                        @endif
+                    </button>
+                    <!-- Dropdown Menu -->
+                    <div class="hidden z-50 my-4 text-base list-none bg-white rounded-lg divide-y divide-gray-100 shadow-sm dark:bg-gray-700"
+                        id="language-dropdown-menu">
+                        <ul class="py-2 font-medium" role="none">
+                            <li>
+                                <a href="{{ route('language.switch', 'en') }}"
+                                    class="flex justify-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    role="menuitem">
+                                    <svg class="w-5 h-5 rounded-full" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3900 3900">
+                                        <path fill="#b22234" d="M0 0h7410v3900H0z" />
+                                        <path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
+                                            stroke="#fff" stroke-width="300" />
+                                        <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
+                                        <g fill="#fff">
+                                            <use xlink:href="#d" x="988" />
+                                            <use xlink:href="#c" x="1976" />
+                                            <use xlink:href="#e" x="2470" />
+                                        </g>
+                                    </svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('language.switch', 'id') }}"
+                                    class="flex justify-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    role="menuitem">
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg"
+                                        class="w-5 h-5 rounded-full" alt="indonesia">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="flex items-center ms-3">
+
+                <!-- Dark Mode Toggle -->
+                <button id="theme-toggle" type="button"
+                    class="p-2 text-sm text-gray-500 rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+                    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5 text-gray-800 dark:text-white"
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                            d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z"
+                            clip-rule="evenodd" />
+                    </svg>
+
+                    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5 text-gray-800 dark:text-white"
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                            d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </button>
+
+                <!-- User Profile -->
+                <div class="flex items-center">
                     <div>
                         <button type="button"
                             class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
@@ -57,91 +146,6 @@
                         </ul>
                     </div>
                 </div>
-                {{-- Language - Switch --}}
-                <div class="relative">
-                    <button type="button" data-dropdown-toggle="language-dropdown-menu"
-                        class="inline-flex justify-center items-center px-4 py-2 ml-2 text-sm font-medium text-gray-900 rounded-lg cursor-pointer dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
-                        @if (app()->getLocale() == 'en')
-                            <svg class="w-5 h-5 rounded-full me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 3900 3900">
-                                <path fill="#b22234" d="M0 0h7410v3900H0z" />
-                                <path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff"
-                                    stroke-width="300" />
-                                <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
-                                <g fill="#fff">
-                                    <g id="d">
-                                        <g id="c">
-                                            <g id="e">
-                                                <g id="b">
-                                                    <path id="a"
-                                                        d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z" />
-                                                    <use xlink:href="#a" y="420" />
-                                                    <use xlink:href="#a" y="840" />
-                                                    <use xlink:href="#a" y="1260" />
-                                                </g>
-                                                <use xlink:href="#a" y="1680" />
-                                            </g>
-                                            <use xlink:href="#b" x="247" y="210" />
-                                        </g>
-                                        <use xlink:href="#c" x="494" />
-                                    </g>
-                                    <use xlink:href="#d" x="988" />
-                                    <use xlink:href="#c" x="1976" />
-                                    <use xlink:href="#e" x="2470" />
-                                </g>
-                            </svg>
-                        @else
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg"
-                                class="w-5 h-5 rounded-full me-3" alt="indonesia">
-                        @endif
-                        {{ strtoupper(app()->getLocale()) }}
-                    </button>
-                    <!-- Dropdown -->
-                    <div class="hidden z-50 my-4 text-base list-none bg-white rounded-lg divide-y divide-gray-100 shadow-sm dark:bg-gray-700"
-                        id="language-dropdown-menu">
-                        <ul class="py-2 font-medium" role="none">
-                            <li>
-                                <a href="{{ route('language.switch', 'en') }}"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    role="menuitem">
-                                    <div class="inline-flex items-center">
-                                        <svg aria-hidden="true" class="w-3.5 h-3.5 rounded-full me-2"
-                                            xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-us"
-                                            viewBox="0 0 512 512">
-                                            <g fill-rule="evenodd">
-                                                <g stroke-width="1pt">
-                                                    <path fill="#bd3d44"
-                                                        d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                                                        transform="scale(3.9385)" />
-                                                    <path fill="#fff"
-                                                        d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                                                        transform="scale(3.9385)" />
-                                                </g>
-                                                <path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)" />
-                                                <path fill="#fff"
-                                                    d="M8.2 3l1 2.8H12L9.7 7.5l.9 2.7-2.4-1.7L6 10.2l.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7L74 8.5l-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 7.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9z"
-                                                    transform="scale(3.9385)" />
-                                            </g>
-                                        </svg>
-                                        EN
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('language.switch', 'id') }}"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    role="menuitem">
-                                    <div class="inline-flex items-center">
-                                        <img src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg"
-                                            class="w-3.5 h-3.5 rounded-full me-2" alt="indonesia">
-                                        ID
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                {{-- End Language - Switch --}}
             </div>
         </div>
     </div>
